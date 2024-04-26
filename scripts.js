004bed7ddfd57afb607b85a13c839a4d0c904176
lqiuuu <lq2112@nyu.edu>
2024-04-25 20:32:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let drinkName;
 let list;
+let message;
 async function getData() {
   try{
     const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
@@ -21,38 +22,43 @@ async function getData() {
       // console.log(drink)
       const name =  data.drinks[0].strDrink
       const glass = data.drinks[0].strGlass
+
+      message = `${name} in a ${glass}`
+      console.log(message)
       // console.log(name)
       // drinkNames.push(name);
       // console.log(drinkNames)
-      return {name, glass}
+      // return {name, glass}
+      return message
     }
     
     console.log(response.status);
     console.log(response.statusText);
-
-    // let drinkName = 
-    
-  
   }
   catch(err){
     console.log("something is wrong. " + err)
   }
   }
+  console.log(message);
   // var data = JSON.parse(this.response)
   // data.forEach((drink) => {
   //   // Log each movie's title
   //   console.log(drink.strDrink)
   // })
 getData().then(function (result){
-  drinkName = result
-  console.log(drinkName);
-  list = Object.entries(drinkName);
-  let content = `<h1> ${list[0][1]} in a ${list[1][1]}</h1>`
-  console.log(list);
-  document.getElementById("name").innerHTML = content;
+  // drinkName = result
+  // console.log(drinkName);
+  // list = Object.entries(drinkName);
+  // let content = `<h1> ${list[0][1]} in a ${list[1][1]}</h1>`
+  // console.log(list);
+  document.getElementById("name").innerHTML = result;
 })
 
+document.getElementById("pour").onClick = refresh
 
+function refresh(){
+  getData()
+}
 
 // 
 
